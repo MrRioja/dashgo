c7a19eb337a5abb961fce9c5a83fdd168c8460b9
Luiz Rioja <lulyrioja@gmail.com>
2022-08-02 20:40:42 -0300

    chore: add mutation to create user in mirage

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -16,6 +16,10 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { useMutation } from "@tanstack/react-query";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -37,6 +41,26 @@ const createUserFormSchema = yup.object({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["users"]);
+      },
+    }
+  );
+
   const {
     register,
     handleSubmit,
@@ -46,9 +70,9 @@ export default function CreateUser() {
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
     values
   ) => {
-    await new Promise((resolve) => setTimeout(resolve, 200));
+    await createUser.mutateAsync(values);
 
-    console.log(values);
+    router.push("/users");
   };
 
   return (
